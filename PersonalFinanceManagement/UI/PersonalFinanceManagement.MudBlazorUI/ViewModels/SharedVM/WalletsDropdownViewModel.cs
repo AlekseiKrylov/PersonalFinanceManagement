be75d5d2c334fa762a26d9d34557f164dfaff995
Alexei Krylov <alexei.i.krylov@gmail.com>
2023-09-01 13:45:11 +0400

    Saving some experiments before deleting :)

---
+++ PersonalFinanceManagement/UI/PersonalFinanceManagement.MudBlazorUI/ViewModels/SharedVM/WalletsDropdownViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using PersonalFinanceManagement.Domain.DTOModels;
+using PersonalFinanceManagement.Interfaces.WebApiClients;
+
+namespace PersonalFinanceManagement.MudBlazorUI.ViewModels.SharedVM
+{
+    public class WalletsDropdownViewModel : ComponentBase
+    {
+        protected List<WalletDTO> _wallets = new();
+
+        [Inject] IEntitiesWebApiClient<WalletDTO, WalletCreateDTO> WalletsWebAtiClient { get; init; }
+        [Parameter] public WalletDTO SelectedWallet { get; set; }
+        [Parameter] public Func<WalletDTO, Task> OnWalletChamged { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _wallets = (await WalletsWebAtiClient.GetAllAsync()).ToList();
+        }
+
+        protected async Task WalletChanged(WalletDTO wallet)
+        {
+            SelectedWallet = wallet;
+            await OnWalletChamged(wallet);
+        }
+    }
+}
